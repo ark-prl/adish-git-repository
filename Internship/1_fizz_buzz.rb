8959ba24f39c476196dac4b917bd184246f5e8b8
Arthur P <arthur.parala@gmail.com>
2020-03-25 10:47:39 +0800

    Uploading answer to fizzbuzz

--- Internship/1_fizz_buzz.rb
+++ Internship/1_fizz_buzz.rb
@@ -5,3 +5,17 @@
 #
 puts "FizzBuzz"
 
+count = 1
+
+while count <= 15
+    if count % 3 == 0 && count % 5 == 0
+        puts "FizzBuzz"    
+    elsif count % 3 == 0
+        puts "Fizz"
+    elsif count % 5 == 0
+        puts "Buzz"
+    else
+        puts count
+    end
+    count += 1
+end
